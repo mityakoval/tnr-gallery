5d62372dae831959dc7e7846c4d2424d76cec1ef
Mitya Koval <dima.alex.koval@gmail.com>
2020-01-30 18:36:18 +0100

    fix
    Signed-off-by: Mitya Koval <dima.alex.koval@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module TnrGallery
     # Don't generate system test files.
     config.generators.system_tests = nil
     
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource(
