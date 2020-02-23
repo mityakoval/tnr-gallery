7be909c2a0ebbaba455c78daa6bbee0dd018af60
Mitya Koval <dima.alex.koval@gmail.com>
2020-02-23 13:55:54 +0100

    use mina for deployment
    Signed-off-by: Mitya Koval <dima.alex.koval@gmail.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,102 +1,88 @@
-set :repo_url,        'git@github.com:mityakoval/tnr-gallery.git'
-set :application,     'tnr-gallery'
-set :user,            'deploy'
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
+require 'mina/rails'
+require 'mina/git'
+# require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
+require 'mina/rvm'    # for rvm support. (https://rvm.io)
 
-# Don't change these unless you know what you're doing
-set :pty,             false
-set :use_sudo,        false
-set :stage,           :production
-set :env,             :production
-set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, false  # Change to false when not using ActiveRecord
+# Basic settings:
+#   domain       - The hostname to SSH to.
+#   deploy_to    - Path to deploy into.
+#   repository   - Git repo to clone from. (needed by mina/git)
+#   branch       - Branch name to deploy. (needed by mina/git)
 
-# Sidekiq configs
-# set :sidekiq_config, -> {File.join(release_path, 'config', 'sidekiq.yml')}
-# set :sidekiq_log, -> {File.join(release_path, 'log', 'sidekiq.log')}
+set :application_name, 'tnr-gallery'
+set :domain, '138.68.100.165'
+set :deploy_to, '/home/deploy/apps/tnr-gallery'
+set :repository, 'git@github.com:mityakoval/tnr-gallery.git'
+set :branch, 'master'
 
-# Other configs
-set :backup_path, "#{release_path}/config/backup"
+# Optional settings:
+set :user, 'deploy'          # Username in the server to SSH to.
+set :port, '22'              # SSH port number.
+set :forward_agent, true     # SSH forward_agent.
 
-## Defaults:
-# set :scm,           :git
-# set :branch,        :master
-# set :format,        :pretty
-# set :log_level,     :debug
-# set :keep_releases, 5
+# Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
+# Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
+# run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
+# set :shared_dirs, fetch(:shared_dirs, []).push('public/assets')
+# set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
 
-## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, fetch(:linked_dirs, []).push('public/system')
+# This task is the environment that is loaded for all remote run commands, such as
+# `mina deploy` or `mina rake`.
+task :remote_environment do
+  # If you're using rbenv, use this to load the rbenv environment.
+  # Be sure to commit your .ruby-version or .rbenv-version to your repository.
+  # invoke :'rbenv:load'
 
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-
-  before :start, :make_dirs
+  # For those using RVM, use this to load an RVM version@gemset.
+  invoke :'rvm:use', 'default'
 end
 
-namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
+# Put any custom commands you need to run at setup
+# All paths in `shared_dirs` and `shared_paths` will be created on their own.
+task :setup do
+  # command %{rbenv install 2.3.0 --skip-existing}
+end
+
+namespace :credentials do
+  task :upload do
+    run :local do
+      command "scp config/master.key #{fetch(:user)}@#{fetch(:domain)}:#{fetch(:shared_path)}/"
+    end
+  end
+  
+  task :symlink do
+    command "ln -sf #{fetch(:shared_path)}/master.key #{fetch(:current_path)}/config/master.key"
+  end
+end
+
+desc "Deploys the current version to the server."
+task :deploy do
+  # uncomment this line to make sure you pushed your local branch to the remote origin
+  # invoke :'git:ensure_pushed'
+  invoke :'credentials:upload'
+  deploy do
+    # Put things that will set up an empty directory into a fully set-up
+    # instance of your project.
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle:install'
+    invoke :'credentials:symlink'
+    invoke :'rails:db_migrate'
+    invoke :'rails:assets_precompile'
+    invoke :'deploy:cleanup'
+
+    on :launch do
+      in_path(fetch(:current_path)) do
+        command %{mkdir -p tmp/}
+        command %{touch tmp/restart.txt}
       end
     end
   end
 
-  desc 'Initial Deploy'
-  task :initial do
-    on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
-    end
-  end
-
-  # before :starting,     :check_revision
-  after  :finishing,    :compile_assets
-  after  :finishing,    :cleanup
+  # you can use `run :local` to run tasks on local machine before of after the deploy scripts
+  # run(:local){ say 'done' }
 end
 
-namespace :credentials do
-  desc "SCP transfer figaro configuration to the shared folder"
-  task :setup do
-    on roles(:app) do
-      upload! "config/master.key", "#{shared_path}/master.key", via: :scp
-    end
-  end
-
-  desc "Symlink application.yml to the release path"
-  task :symlink do
-    on roles(:app) do
-      execute "test -f #{release_path}/config/master.key || ln -sf #{shared_path}/master.key #{release_path}/config/master.key"
-    end
-  end
-
-  after  'deploy:started',  'credentials:setup'
-  after  'deploy:updating', 'credentials:symlink'
-end
-
-
-# ps aux | grep puma    # Get puma pid
-# kill -s SIGUSR2 pid   # Restart puma
-# kill -s SIGTERM pid   # Stop puma
+# For help in making your deploy script, see the Mina documentation:
+#
+#  - https://github.com/mina-deploy/mina/tree/master/docs
